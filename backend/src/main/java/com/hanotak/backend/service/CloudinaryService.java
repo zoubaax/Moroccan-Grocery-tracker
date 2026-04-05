5951fdc6ee6776a6ea16c2914d625d2d049d5f71
zoubaa mohammed <zoubaax@gmail.com>
2026-04-05 19:34:29 +0100

    feat: initialize backend project with Spring Boot, PostgreSQL, JWT, and Cloudinary integration

---
+++ backend/src/main/java/com/hanotak/backend/service/CloudinaryService.java
@@ -0,0 +1,21 @@
+package com.hanotak.backend.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public Map uploadImage(MultipartFile file) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
+    }
+}
